3971fe657e90abdc8e0d9f363fb07431b9339847
Jose Lucas <lucasnba2324@gmail.com>
2023-12-06 14:01:27 -0300

    config list show update updatePartal

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { CreateUserDTO } from "./dto/create-user-dto";
 import { PrismaService } from "src/prisma/prisma.service";
+import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
+import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
 
 @Injectable()
 export class UserService {
@@ -12,6 +14,56 @@ export class UserService {
         email,
         name,
         password
+      }
+    });
+  }
+
+  async list() {
+    return this.prisma.user.findMany();
+  }
+
+  async show(id: number) {
+    return this.prisma.user.findUnique({
+      where: {
+        id
+      }
+    });
+  }
+
+  async update(id: number, { name, email, password, birthday }: UpdatePutUserDTO) {
+    return this.prisma.user.update({
+      data: { name, email, password, birthday: birthday ? new Date(birthday) : null },
+      where: {
+        id
+      }
+    });
+  }
+
+  async updatePartial(id: number, { name, email, password, birthday }: UpdatePatchUserDTO) {
+    const data: any = {};
+
+    if (birthday) {
+      data.birthday = new Date(birthday);
+    }
+
+    if (email) {
+      data.email = email;
+    }
+
+
+    if (name) {
+      data.name = name;
+    }
+
+
+    if (password) {
+      data.password = password;
+    }
+
+    return this.prisma.user.update({
+      data,
+      where: {
+        id
       }
     });
   }
