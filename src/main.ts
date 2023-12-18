05781c26066197656fe87fc14a6f0fbcb9846b94
Jose Lucas <lucasnba2324@gmail.com>
2023-12-18 08:05:11 -0300

    config cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { LogInterceptor } from './interceptors/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(
+    // {methods: ['get'], origin: ['http://localhost:teste']}
+  );
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LogInterceptor());
   await app.listen(3004);
