792d51e92fb5fa6b7853cf663621a7826c6f87b8
Jose Lucas <lucasnba2324@gmail.com>
2023-12-22 18:54:23 -0300

    importing TypeORM module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,15 +4,18 @@ import { AuthController } from "./auth.controller";
 import { UserModule } from "src/user/user.module";
 import { AuthService } from "./auth.service";
 import { FileModule } from "src/file/file.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserEntity } from "src/user/entity/user.entity";
 
 
 @Module({
   imports: [
     JwtModule.register({
-      secret: process.env.JWT_SECRET_KEY,
+      secret: String(process.env.JWT_SECRET_KEY),
     }),
     forwardRef(() => UserModule),
-    FileModule
+    FileModule,
+    TypeOrmModule.forFeature([UserEntity])
   ],
   controllers: [AuthController],
   providers: [AuthService],
