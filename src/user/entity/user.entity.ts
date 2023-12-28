90dd3d6e74eda69c398dd45e1dcfe4299d69274f
Jose Lucas <lucasnba2324@gmail.com>
2023-12-28 11:25:58 -0300

    fixing import

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,4 +1,4 @@
-import { Role } from "src/enums/role.enum";
+import { Role } from "../../enums/role.enum";
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity({
