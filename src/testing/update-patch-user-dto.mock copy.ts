d15279c48523a71ead67c85c1c997795e1fef818
Jose Lucas <lucasnba2324@gmail.com>
2023-12-29 17:13:52 -0300

    updates mocks

---
+++ src/testing/update-patch-user-dto.mock copy.ts
@@ -0,0 +1,8 @@
+
+import { UpdatePatchUserDTO } from "../user/dto/update-patch-user.dto";
+import { Role } from "../enums/role.enum";
+
+
+export const updatePatchUserDTO: UpdatePatchUserDTO = {
+  name: 'Wilson'
+}
