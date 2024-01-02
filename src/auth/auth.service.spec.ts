be8d6a0fa991f50e266ff15df62e522db8dd6c0f
Jose Lucas <lucasnba2324@gmail.com>
2024-01-02 17:30:07 -0300

    creating auth service spec testing

---
+++ src/auth/auth.service.spec.ts
@@ -0,0 +1,82 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { AuthService } from "./auth.service";
+import { userRepositoryMock } from "../testing/user-repository.mock";
+import { jwtServiceMock } from "../testing/jwt-service.mock";
+import { userServiceMock } from "../testing/user-service.mock";
+import { mailerServiceMock } from "../testing/mailer-service.mock";
+import { userEntityList } from "../testing/user-entity-list.mock";
+import { accessToken } from "../testing/access-token.mock";
+import { jwtPayload } from "../testing/jwt-payload.mock";
+import { resetToken } from "../testing/reset-token.mock";
+import { authRegisterDTO } from "../testing/auth-register-dto.mock";
+
+describe('AuthService', () => {
+  let authService: AuthService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        userRepositoryMock,
+        jwtServiceMock,
+        userServiceMock,
+        mailerServiceMock
+      ]
+    }).compile();
+
+    authService = module.get<AuthService>(AuthService);
+  });
+
+  it('Validate the definition', () => {
+    expect(authService).toBeDefined();
+
+  });
+
+  describe('Token', () => {
+    it('createToken', () => {
+      const result = authService.createToken(userEntityList[0]);
+
+      expect(result).toEqual({
+        accessToken
+      });
+    });
+
+    it('checkToken', () => {
+      const result = authService.checkToken(accessToken);
+
+      expect(result).toEqual(jwtPayload);
+    });
+
+    it('isValidToken', () => {
+      const result = authService.isValidToken(accessToken);
+
+      expect(result).toEqual(true);
+    });
+  });
+
+  describe('Autenticação', () => {
+    it('Login', async () => {
+      const result = await authService.login('ingrid@gmail.com', '123456');
+
+      expect(result).toEqual({ accessToken })
+    });
+
+    it('Forget', async () => {
+      const result = await authService.forget('ingrid@gmail.com');
+
+      expect(result).toEqual(true)
+    });
+
+    it('Reset', async () => {
+      const result = await authService.reset('654321', resetToken);
+
+      expect(result).toEqual({ accessToken });
+    });
+
+    it('Register', async () => {
+      const result = await authService.register(authRegisterDTO);
+
+      expect(result).toEqual({ accessToken });
+    });
+  });
+})
