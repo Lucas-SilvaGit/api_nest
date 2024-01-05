1ad6ea071949c959c798529eb6fb5d3cf1ae7c56
Jose Lucas <lucasnba2324@gmail.com>
2024-01-05 15:37:55 -0300

    creating file service test

---
+++ src/file/file.service.spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { FileService } from "./file.service";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import { getPhoto } from "../testing/get-photo.mock";
+
+describe('FileService', () => {
+
+  let fileService: FileService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [FileService]
+    }).compile();
+
+    fileService = module.get<FileService>(FileService);
+  });
+
+  it('Validar a definição', () => {
+    expect(fileService).toBeDefined();
+  });
+
+  describe('Teste do File Service', () => {
+    it('upload method', async () => {
+      const photo = await getPhoto();
+      const filename = 'photo-test.png';
+
+      fileService.upload(photo, filename)
+    });
+  })
+})
